921918c3eb2d9a71c569540379337ea008794f7f
Gabriela Fernandes <gabriela.cavalcanti886@gmail.com>
2024-05-28 16:47:05 -0300

    feat: admin endpoints

--- app.js
+++ app.js
@@ -10,14 +10,114 @@ const express = require('express')
 const cors = require('cors')
 const bodyParser = require('body-parser')
 
-/****************************** IMPORT DE CONTROLLERS ****************************/
 
 
 // criando objeto app
 const app = express()
+
 app.use((request, response, next) => {
-    response.header('Acess-Control-Allow-Origin', '*')
-    response.header('Acess-Control-Allow-Methods', '*')
+    response.header('Access-Control-Allow-Origin', '*')
+    response.header('Access-Control-Allow-Methods', '*')
     app.use(cors())
+    // app.use(express.json());
     next()
+})
+
+// cria um objeto do tipo JSON para receber os dados via body nas requisições POST ou PUT
+const bodyParserJSON = bodyParser.json()
+
+/****************************** IMPORT DE CONTROLLERS ****************************/
+const controllerAdmin = require('./controller/controller-administrator.js')
+/*********************************************************************************/
+
+// #region ADMIN
+
+/****************************** ADMINISTRADOR ****************************/
+// endpoints: listar os admins
+app.get('/v1/leilao_expresso/admins', cors(), async(request, response, next) => {
+    // chama a função para retornar os dados do admin
+    let dadosAdmins = await controllerAdmin.getListarAdmins()
+
+    response.status(dadosAdmins.status_code)
+    response.json(dadosAdmins)
+})
+
+// endpoint: filtrar pelo nome
+app.get('/v1/leilao_expresso/admins/filtro', cors(), async(request, response, next) => {
+    let filtro = request.query.nome
+
+    // chama a função para retornar os dados do admin
+    let dadosAdmins = await controllerAdmin.getAdminByNome(filtro)
+
+    response.status(dadosAdmins.status_code)
+    response.json(dadosAdmins)
+})
+
+// endpoint: retorna os dados do admin, filtrando pelo ID
+app.get('/v1/leilao_expresso/admin/:id', cors(), async(request, response, next) => {
+    // recebe o id da requisição do admin
+    let idAdmin = request.params.id
+
+    let dadosAdmin = await controllerAdmin.getBuscarAdmin(idAdmin)
+
+    response.status(dadosAdmin.status_code)
+    response.json(dadosAdmin)
+})
+
+// endpoint: inserir novos admins no Banco de Dados
+    // não esquecer de colocar o bodyParserJSON que é quem define o formato de chegada dos dados
+app.post('/v1/leilao_expresso/admin', cors(), bodyParserJSON, async(request, response, next) => {
+
+        // recebe o content type da requisição (A API deve receber somente application/json)
+        let contentType = request.headers['content-type']
+    
+        //recebe os dados encaminhados na requisição no body(JSON)
+        let dadosBody = request.body
+    
+        // encaminha os dados da requisição para a controller enviar para o BD
+        let resultDados = await controllerAdmin.setNovoAdmin(dadosBody, contentType)
+        
+        response.status(resultDados.status_code)
+        response.json(resultDados)
+    
+})
+
+// endpoint: editar o status do admin para false para "exclui-lo"
+app.put('/v1/leilao_expresso/admin/excluir/:id', cors(), async(request, response, next) => {
+    let admin = request.params.id
+    let dadosAdmin = await controllerAdmin.setEditarExcluirAdmin(admin)
+
+    response.status(dadosAdmin.status_code)
+    response.json(dadosAdmin)
+})
+
+// endpoint: editar o status do admin para false para acha-lo
+app.put('/v1/leilao_expresso/admin/ativar/:id', cors(), async(request, response, next) => {
+    let admin = request.params.id
+    let dadosAdmin = await controllerAdmin.setEditarRenovarAdmin(admin)
+
+    response.status(dadosAdmin.status_code)
+    response.json(dadosAdmin)
+})
+
+// endpoint: editar os dados do admin
+app.put('/v1/leilao_expresso/admin/:id', cors(), bodyParserJSON, async(request, response, next) => {
+    let admin = request.params.id
+
+    // recebe o content type da requisição (A API deve receber somente application/json)
+    let contentType = request.headers['content-type']
+
+    //recebe os dados encaminhados na requisição no body(JSON)
+    let dadosBody = request.body
+
+    // encaminha os dados da requisição para a controller enviar para o BD
+    let resultDados = await controllerAdmin.setAtualizarAdmin(dadosBody, contentType, admin)
+    
+    response.status(resultDados.status_code)
+    response.json(resultDados)
+})
+/*************************************************************************/
+
+app.listen(8080, () => {
+    console.log('API rodando na porta 8080.')
 })
